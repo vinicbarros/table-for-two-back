2bfcc1631356733cbc4d45fcd13b91a4246e745b
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-05-16 11:23:54 -0300

    feat: created a handling error middleware

---
+++ src/middlewares/error-handling-middleware.js
@@ -0,0 +1,46 @@
+import httpStatus from "http-status";
+
+export function handleApplicationErrors(err, req, res, next) {
+  if (err.name === "BadRequestError") {
+    return res.status(httpStatus.BAD_REQUEST).send({
+      message: err.message,
+    });
+  }
+
+  if (err.name === "DuplicatedEmailError") {
+    return res.status(httpStatus.CONFLICT).send({
+      message: err.message,
+    });
+  }
+
+  if (err.name == "InvalidCredentialsError") {
+    return res.status(httpStatus.UNAUTHORIZED).send({
+      message: err.message,
+    });
+  }
+
+  if (err.name === "ConflictError") {
+    return res.status(httpStatus.CONFLICT).send({
+      message: err.message,
+    });
+  }
+
+  if (err.name === "NotFoundError") {
+    return res.status(httpStatus.NOT_FOUND).send({
+      message: err.message,
+    });
+  }
+
+  if (err.name === "UnauthorizedError") {
+    return res.status(httpStatus.UNAUTHORIZED).send({
+      message: err.message,
+    });
+  }
+
+  res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+    error: "InternalServerError",
+    message: "Internal Server Error",
+  });
+
+  return next();
+}
